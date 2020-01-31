ad5953ff6191fe5cbbeb3881c8729e40967b344f
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-31 16:36:26 +0300

    added Editor test class

--- photo/tests.py
+++ photo/tests.py
@@ -2,3 +2,11 @@ from django.test import TestCase
 from .models import Editor, Category, Location, Image
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.brian= Editor(first_name = 'Brian', last_name ='Mutuma', email ='mutuma@gmail.com')
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.brian,Editor))
